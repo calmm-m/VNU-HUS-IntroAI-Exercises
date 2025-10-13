4a0cea323ba751f91a4f34ed990f8d1ce6994894
Nguyễn Hữu Quyết <ilysm.orenji@gmail.com>
2025-10-13 10:57:33 +0700

    Update ones.pl

--- Chapter 5/ones.pl
+++ Chapter 5/ones.pl
@@ -1,1 +1,10 @@
 % Create a solution for Exercise 5.5, page 88
+% In ra 1024 số 1
+print_ones(0).
+print_ones(N) :-
+    N > 0,
+    write(1), nl,
+    N1 is N - 1,
+    print_ones(N1).
+
+% Gọi: ?- print_ones(1024).
