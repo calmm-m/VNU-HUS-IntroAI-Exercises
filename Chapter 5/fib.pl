bf427d88ccdac20e203852a62ea8f423846b75ae
Nguyễn Hữu Quyết <ilysm.orenji@gmail.com>
2025-10-13 10:56:00 +0700

    Update fib.pl

--- Chapter 5/fib.pl
+++ Chapter 5/fib.pl
@@ -1,1 +1,11 @@
 % Create a solution for Exercise 5.8(a)
+% fib(N, R): R là số Fibonacci thứ N
+fib(0, 1).
+fib(1, 1).
+fib(N, R) :-
+    N > 1,
+    N1 is N - 1,
+    N2 is N - 2,
+    fib(N1, R1),
+    fib(N2, R2),
+    R is R1 + R2.
